ec731bbf52a42b033e2494bd8e61834613ab33c6
Eleftherios Avramidis <el.avramidis@gmail.com>
2019-11-25 17:33:49 +0000

    Added initial Model class

--- include/Model.hpp
+++ include/Model.hpp
@@ -5,4 +5,15 @@
 #ifndef STATIONSIM_MODEL_HPP
 #define STATIONSIM_MODEL_HPP
 
+#include "stationsim_export.h"
+
+namespace station_sim {
+    class STATIONSIM_EXPORT Model {
+
+            Model();
+
+            ~Model();
+    };
+}
+
 #endif //STATIONSIM_MODEL_HPP
--- source/Model.cpp
+++ source/Model.cpp
@@ -2,3 +2,11 @@
 // Created by ea461 on 25/11/2019.
 //
 
+#include "Model.hpp"
+
+namespace station_sim {
+
+    Model::Model() = default;
+
+    Model::~Model() = default;
+}
