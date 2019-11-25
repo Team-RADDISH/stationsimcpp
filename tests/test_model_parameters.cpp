5021f946b6d8bbe48c274101b7836faa7a90da29
Eleftherios Avramidis <el.avramidis@gmail.com>
2019-11-25 17:34:07 +0000

    Added initial test code for ModelParameters class

---
+++ tests/test_model_parameters.cpp
@@ -0,0 +1,24 @@
+//
+// Created by ea461 on 25/11/2019.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "ModelParameters.hpp"
+
+TEST_CASE("Test ModelParameters constructor") {
+    station_sim::ModelParameters modelparameters;
+
+    SECTION("Test default values") {
+
+    }
+}
+
+TEST_CASE("Test ModelParameters setters exceptions") {
+	station_sim::ModelParameters modelparameters;
+
+	SECTION("Test default values") {
+
+	}
+}
