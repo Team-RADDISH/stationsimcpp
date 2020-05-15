6a83e763d84db59eeb12ea9011b5d811bcc19aad
Eleftherios Avramidis <el.avramidis@gmail.com>
2020-05-15 08:09:17 +0100

    Code formatting

--- source/model/Agent.cpp
+++ source/model/Agent.cpp
@@ -149,7 +149,6 @@ namespace station_sim {
 
         return Point2D((desired_location.x - agent_location.x) / distance,
                        (desired_location.y - agent_location.y) / distance);
-        ;
     }
 
     float Agent::calculate_distance(Point2D location_0, Point2D location_1) {
