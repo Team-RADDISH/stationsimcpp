6417620593b058dcc7cb27dad0e2cebb87a97510
Eleftherios Avramidis <el.avramidis@gmail.com>
2020-06-11 10:26:24 +0100

    Renamed variable for clarity

--- source/model/Model.cpp
+++ source/model/Model.cpp
@@ -60,7 +60,7 @@ namespace station_sim {
     }
 
     void Model::initialize_model(int unique_id) {
-        generator = std::make_shared<std::mt19937>(std::mt19937(model_parameters.get_random_seed()));
+        random_number_generator = std::make_shared<std::mt19937>(std::mt19937(model_parameters.get_random_seed()));
 
         model_id = unique_id;
         status = ModelStatus::active;
@@ -175,7 +175,7 @@ namespace station_sim {
 
     float Model::get_speed_step() const { return speed_step; }
 
-    std::shared_ptr<std::mt19937> Model::get_generator() const { return generator; }
+    std::shared_ptr<std::mt19937> Model::get_generator() const { return random_number_generator; }
 
     std::vector<Point2D> Model::get_agents_location() {
         std::vector<Point2D> agents_locations;
@@ -308,7 +308,7 @@ namespace station_sim {
 
     void Model::reseed_random_number_generator() {
         std::random_device r;
-        generator = std::make_shared<std::mt19937>(std::mt19937(r()));
+        random_number_generator = std::make_shared<std::mt19937>(std::mt19937(r()));
     }
 
     const ModelState Model::get_state() const {
